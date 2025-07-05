41e4705b3474ea25384e7d06836044a579dcf2c2
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-07-05 21:14:42 +0600

    cors error solved

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,14 @@ import { bookRoutes } from "./app/controller/book.controller";
 import { borrowRoutes } from "./app/controller/borrow.controller";
 import cors from "cors";
 const app: Application = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://library-management-topaz.vercel.app",
+    ],
+  })
+);
 app.use(express.json());
 app.use("/api", bookRoutes);
 app.use("/api", borrowRoutes);
