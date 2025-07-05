e4cf5ebabe1b0a6432cba124a3c978aec8c234d8
RAKIBURRAHMAN007 <ratulrakibur5@gmail.com>
2025-07-05 13:21:45 +0600

    crs added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { Application } from "express";
 import { bookRoutes } from "./app/controller/book.controller";
 import { borrowRoutes } from "./app/controller/borrow.controller";
+import cors from "cors";
 const app: Application = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", bookRoutes);
 app.use("/api", borrowRoutes);
